71197800ead5989937086694904f08302ffc73a6
EvgeniyaG11 <konunh@gmail.com>
2020-10-09 16:24:17 +0300

    Commit message

---
+++ IF ili ne IF.py
@@ -0,0 +1,8 @@
+q = int(input("enter the number: "))
+a = int(input("enter the number: "))
+if q > a:
+    print("На берегу пруда. If или If? Вот в чём вопрос...")
+elif q == a:
+    print("Ленив самурай — Неделю танто пинал; Упущен дедлайн")
+elif q < a:
+    print("Солнца еще нет; Но сосед Дрель-сан не спит; Вот чёртов гайдзин")
