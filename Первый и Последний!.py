4a8aad57407ba508456aa74e0f44b0517347fb9c
EvgeniyaG11 <72605038+EvgeniyaG11@users.noreply.github.com>
2020-10-18 22:55:29 +0300

    Update Первый и Последний!.py

--- Первый и Последний!.py
+++ Первый и Последний!.py
@@ -6,11 +6,11 @@ def first(a):
     return a[0]
 
 
-print("First number at list: " + str(a[0]))
+print("Last number at list: " + str(last(a)))
 
 
 def last(a):
     return a[-1]
 
 
-print("Last number at list: " + str(a[-1]))
+print("Last number at list: " + str(last(a)))
